1cbc454eaf67606b0a149f5adec7fdd2c10ab32d
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-01-20 14:33:02 +0200

    Uygulama MakeMigration

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    yazar = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    baslik = models.CharField(max_length=200)
+    yazi = models.TextField()
+    olus_tar = models.DateTimeField(
+            default=timezone.now)
+    yayim_tar = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.yayim_tar = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
